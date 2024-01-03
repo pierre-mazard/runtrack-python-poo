666930f25855b244d10db1115e5dfbdb3ce3f3dc
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2024-01-03 12:00:29 +0100

    Job01 ===> Modification erreur lors du push du job02

--- jour01/job01.py
+++ jour01/job01.py
@@ -6,13 +6,12 @@ Created on Wed Jan  3 11:10:14 2024
 """
 
 class Operation:
-    def __init__(self, nombre1=12, nombre2=3):
+    def __init__(self, nombre1=0, nombre2=0):
         self.nombre1 = nombre1
         self.nombre2 = nombre2
         
 operation = Operation()
 
-print(f"Le nombre1 est {operation.nombre1}")
-print(f"Le nombre2 est {operation.nombre2}")
+print(operation)
 
 
