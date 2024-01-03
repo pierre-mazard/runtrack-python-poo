ddda587c96d0bd9fd9c7edbcc7f362a52f0bbc80
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2024-01-03 11:26:49 +0100

    Job01 ===> Création d'une classe

---
+++ jour01/job01.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  3 11:10:14 2024
+
+@author: Mazard Pierre
+"""
+
+class Operation:
+    def __init__(self, nombre1=0, nombre2=0):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+        
+operation = Operation()
+
+print(operation)
+
+
