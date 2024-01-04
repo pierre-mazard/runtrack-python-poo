0730943c4a1ab53b40bbcc554de8008a2b57daf2
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2024-01-04 14:23:00 +0100

    Job02> ====> Implémentation de l'attribut decouvert a la classe CompteBancaire ayant pour valeur un booléen.

--- jour03/job02.py
+++ jour03/job02.py
@@ -6,14 +6,15 @@ Created on Thu Jan  4 14:07:08 2024
 """
 
 class CompteBancaire:
-    def __init__(self, numero_compte: str, nom: str, prenom: str, solde: float):
+    def __init__(self, numero_compte: str, nom: str, prenom: str, solde: float, decouvert: bool):
         self.__numero_compte = numero_compte
         self.__nom = nom
         self.__prenom = prenom
         self.__solde = solde
+        self.__decouvert = decouvert
 
     def afficher(self):
-        print(f"Numéro de compte: {self.__numero_compte}\nNom: {self.__nom}\nPrénom: {self.__prenom}\nSolde: {self.__solde}")
+        print(f"Numéro de compte: {self.__numero_compte}\nNom: {self.__nom}\nPrénom: {self.__prenom}\nSolde: {self.__solde}\nDécouvert autorisé: {self.__decouvert}")
 
     def afficherSolde(self):
         print(f"Le solde du client est de {self.__solde} euros.")
@@ -23,14 +24,15 @@ class CompteBancaire:
         print(f"Le versement de {montant} euros a été effectué avec succès. Le nouveau solde est de {self.__solde} euros.")
 
     def retrait(self, montant: float):
-        if montant > self.__solde:
-            print("Le montant à retirer est supérieur au solde disponible.")
-        else:
+        if self.__decouvert or montant <= self.__solde:
             self.__solde -= montant
             print(f"Le retrait de {montant} euros a été effectué avec succès. Le nouveau solde est de {self.__solde} euros.")
+        else:
+            print("Le montant à retirer est supérieur au solde disponible et au découvert autorisé.")
 
 
-compte = CompteBancaire("145698741", "Mazard", "Pierre", 1500.66)
+
+compte = CompteBancaire("145698741", "Mazard", "Pierre", 1500.66, True)
 
 compte.afficher()
 compte.afficherSolde()
