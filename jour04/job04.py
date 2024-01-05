4d496b0aa100b5a75bbcd4f9f121d35c5f595624
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2024-01-05 14:09:13 +0100

    Job04 ====> Création de la calsse Rectangle qui hérite de la classe Forme, possédant deux attributs privés largeur et hauteur. ====> Surchage de la méthode aire dans la classe Rectangle.

--- jour04/job04.py
+++ jour04/job04.py
@@ -9,3 +9,11 @@ class Forme:
     def aire(self):
         return 0
     
+class Rectangle(Forme):
+    def __init__(self, largeur, hauteur):
+        self.__largeur = largeur
+        self.__hauteur = hauteur
+    
+    def aire(self):
+        return self.__largeur * self.__hauteur
+    
