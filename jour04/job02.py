17d14a03a7725ae221724ab0c704e6d05a3779b0
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2024-01-05 11:58:01 +0100

    Job02 ====> Reprise du code du job01 ====> Utilisation de la méthode bonjour et allerEnCours pour la classe eleve ====> Redéfinition de l'ĝe de l'élève à 15 ans

---
+++ jour04/job02.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  5 11:53:52 2024
+
+@author: Mazard Pierre
+"""
+
+class Personne:
+    def __init__(self, age= 14):
+        self.age = age
+        
+    def afficherAge(self):
+        print(f"L'âge de la personne est de {self.age} ans.")
+        
+    def bonjour(self):
+        print("Hello")
+        
+    def modifierAge(self, age):
+        self.age = age
+        
+class Eleve(Personne):
+    def allerEnCours(self):
+        print("Je vais en cours")
+    
+    def afficherAge(self):
+        print(f"J'ai {self.age} ans.")
+
+class Professeur:
+    def __init__(self, matiereEnsegnee):
+        self.__matiereEnsegnee = matiereEnsegnee
+    
+    def enseigner(self):
+        print("Le cours va commencer")
+    
+    def getMatiereEnseignee(self):
+        return self.__matiereEnsegnee
+    
+personne = Personne()
+eleve = Eleve()
+
+eleve.modifierAge(15)
+eleve.bonjour()
+eleve.allerEnCours()
