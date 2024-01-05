8be4c4a8e1e86ba91a6c9fa006ca20d48849c7fe
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2024-01-05 15:29:53 +0100

    Job07 ====> Création de la classe Carte avec les attributs valeur et couleur ====> Importation du module random

---
+++ jour04/job07.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  5 14:49:24 2024
+
+@author: Mazard Pierre
+"""
+
+import random
+
+class Carte:
+    def __init(self, valeur, couleur):
+        self.valeur = valeur
+        self.couleur = couleur
+        
+    
