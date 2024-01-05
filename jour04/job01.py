0125ab6fe10dcd9b1fc3329219b33ae0718ce76d
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2024-01-05 11:53:31 +0100

    Job04 ====> Création d'une classe Eleve qui hérite de la classe Personne ====> Ajout de la méthode allerEnCours ====> Ajout de la méthode afficherAge ====> Création de la classe Professeur ayant pour attribut privé matiereEnseignee ====> Ajout de la méthode enseigner
    ====> Instanciation de la classe Personne et Eleve
    ====> Affichage en console de l'âge par défaut de l'élève.

--- jour04/job01.py
+++ jour04/job01.py
@@ -18,3 +18,24 @@ class Personne:
     def modifierAge(self, age):
         self.age = age
         
+class Eleve(Personne):
+    def allerEnCours(self):
+        print("Je vais en cours")
+    
+    def afficherAge(self):
+        print(f"J'ai {self.age} ans.")
+
+class Professeur:
+    def __init__(self, matiereEnsegnee):
+        self.__matiereEnsegnee = matiereEnsegnee
+    
+    def enseigner(self):
+        print("Le cours va commencer")
+    
+    def getMatiereEnseignee(self):
+        return self.__matiereEnsegnee
+    
+personne = Personne()
+eleve = Eleve()
+
+print (f"L'âge par défaut de l'élève est de {eleve.age} ans.")
