3cb492ca51716b1382c5796d4032a57962ad1e6f
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2024-01-05 14:00:18 +0100

    Job03 ====> Création de la classe Parallelepipede héritant de la classe Rectangle ====> Ajout de l'attribut hauteur ====> Ajout de la méthode volume

--- jour04/job03.py
+++ jour04/job03.py
@@ -28,3 +28,18 @@ class Rectangle:
     def set_largeur(self, largeur):
         self.__largeur = largeur
         
+class Parallelepipede(Rectangle):
+    def __init__(self, longueur, largeur, hauteur):
+        super().__init__(longueur, largeur)
+        self.__hauteur = hauteur
+        
+    def volume(self):
+        return self.__longueur * self.__largeur * self.__hauteur
+    
+    def get_hauteur(self):
+        return self.__hauteur
+    
+    def set_hauteur(self, hauteur):
+        self.__hauteur = hauteur
+        
+        
